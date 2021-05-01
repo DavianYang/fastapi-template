9e0f62497c28d74d90f254f993d8a1d4283ee41e
DavianYang <kothantyarzarhein@gmail.com>
2021-05-01 17:40:41 +0630

    [Update] Domain User

--- app/models/domain/users.py
+++ app/models/domain/users.py
@@ -1,24 +1,20 @@
 from __future__ import annotations
 from typing import Optional
 
-from uuid import UUID
-from pydantic import BaseModel, EmailStr, HttpUrl, SecretStr, SecretBytes
-from pydantic.types import constr
-
 from app.models.domain.rwmodel import RWModel
 from app.models.common import DateTimeModelMixin, IDModelMixin
 
 class User(RWModel):
     name: str
     email: str
-    photos: Optional[HttpUrl]
+    photos: Optional[str]
 
     
 class UserInDB(IDModelMixin, DateTimeModelMixin, User):
     salt: str = ""
-    hashed_password: SecretStr = ""
+    hashed_password: str = ""
     
-    def check_password(self, password: SecretStr) -> bool:
+    def check_password(self, password: str) -> bool:
         pass
     
     def change_password(self, password: str) -> None:
