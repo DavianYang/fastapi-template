37e7a8d12f6718eb86a4caf4e578d7cf2fbc3150
DavianYang <kothantyarzarhein@gmail.com>
2021-05-01 17:43:37 +0630

    [Add] Schema User

---
+++ app/models/schemas/rwschema.py
@@ -0,0 +1,5 @@
+from app.models.domain.rwmodel import RWModel
+
+class RWSchema(RWModel):
+    class Config(RWModel.Config):
+        orm_mode = True
---
+++ app/models/schemas/users.py
@@ -0,0 +1,29 @@
+from os import name
+from app.models.domain.users import User
+from app.models.schemas.rwschema import RWSchema
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, HttpUrl
+
+
+class UserInLogin(RWSchema):
+    email: EmailStr
+    password: str
+    
+    
+class UserInCreate(RWSchema):
+    name: str
+    
+    
+class UserInUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    photo: Optional[HttpUrl] = None
+    
+    
+class UserWithToken(User):
+    token: str
+    
+class UserInResponse(RWSchema):
+    user: UserWithToken
