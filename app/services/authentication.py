7c3dd95dde5dace167d4dc3695937a37fc82fd83
DavianYang <kothantyarzarhein@gmail.com>
2021-05-03 17:51:20 +0630

    [Add] Authentication Service

---
+++ app/services/authentication.py
@@ -0,0 +1,18 @@
+from os import truncate
+from app.errors.database import EntityDoesNotExist
+from app.services.users import UserService
+
+async def check_username_is_taken(service: UserService, name: str) -> bool:
+    try:
+        await service.get_user(name=name)
+    except EntityDoesNotExist:
+        return False
+    
+    return True
+
+async def check_email_is_taken(service: UserService, email: str) -> bool:
+    try:
+        await service.get_user_by_email(email=email)
+    except EntityDoesNotExist:
+        return False
+    return True
