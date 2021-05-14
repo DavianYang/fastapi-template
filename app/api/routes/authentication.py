eeeeaac2c9cdfcb770109045e67005ad08cce161
DavianYang <kothantyarzarhein@gmail.com>
2021-05-14 22:15:06 +0630

    [Add] Login

--- app/api/routes/authentication.py
+++ app/api/routes/authentication.py
@@ -2,11 +2,16 @@ from fastapi import APIRouter, Body, Depends, HTTPException
 from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from app.config import settings
-from app.models.schemas.users import UserInCreate, UserInLogin, UserInResponse
+from app.errors.database import EntityDoesNotExist
+from app.models.schemas.users import (
+    UserInCreate,
+    UserInLogin,
+    UserInResponse,
+    UserWithToken,
+)
 from app.resources import strings
 from app.services import jwt
-from app.services.authentication import (check_email_is_taken,
-                                         check_username_is_taken)
+from app.services.authentication import check_email_is_taken, check_username_is_taken
 from app.services.users import UserService
 
 router = APIRouter()
@@ -14,15 +19,32 @@ router = APIRouter()
 
 @router.post("/login", response_model=UserInResponse, name="auth:login")
 async def login(
-    user_login: UserInLogin,
+    user_login: UserInLogin = Body(..., embed=True, alias="user"),
+    user_service: UserService = Depends(UserService),
 ):
-    raise NotImplementedError
+    wrong_login_error = HTTPException(
+        status_code=HTTP_400_BAD_REQUEST, detail=strings.INCORRECT_LOGIN_INPUT
+    )
+
+    try:
+        user = await user_service.get_user_by_email(email=user_login.email)
+    except EntityDoesNotExist as exc_error:
+        raise wrong_login_error from exc_error
+
+    if not user.check_password(user_login.password):
+        raise wrong_login_error
+
+    token = jwt.create_access_token_for_user(user, str(settings.SECRET_KEY))
+
+    return UserInResponse(
+        user=UserWithToken(name=user.name, email=user.email, token=token)
+    )
 
 
 @router.post(
     "",
     status_code=HTTP_201_CREATED,
-    # response_model=UserInResponse,
+    response_model=UserInResponse,
     name="auth:register",
 )
 async def register(
@@ -43,4 +65,6 @@ async def register(
 
     token = jwt.create_access_token_for_user(user, str(settings.SECRET_KEY))
 
-    return token
+    return UserInResponse(
+        user=UserWithToken(name=user.name, email=user.email, token=token)
+    )
--- app/resources/strings.py
+++ app/resources/strings.py
@@ -1,2 +1,3 @@
-USERNAME_TAKEN = "user with this name already exists"
 EMAIL_TAKEN = "email with this email already exists"
+INCORRECT_LOGIN_INPUT = "incorrect email or password"
+USERNAME_TAKEN = "user with this name already exists"
