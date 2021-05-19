3b1534542be80177f3ba6f1a1a97cc43aab05630
DavianYang <kothantyarzarhein@gmail.com>
2021-05-19 21:35:26 +0630

    [Add] Get Current User

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from app.api.dependencies.authentication import get_current_user_authorizer
 from app.config import settings
+from app.models.domain.users import User
 from app.models.schemas.users import UserInResponse, UserWithToken
 from app.services import jwt
 
@@ -8,7 +10,8 @@ router = APIRouter()
 
 
 @router.get("", response_model=UserInResponse, name="users:get-current-user")
-async def retrieve_current_user(user):
+async def retrieve_current_user(user: User = Depends(get_current_user_authorizer())):
+    print("User", user)
     token = jwt.create_access_token_for_user(user, str(settings.SECRET_KEY))
     return UserInResponse(
         user=UserWithToken(name=user.name, email=user.email, token=token)
